be0113c1dd5b955a7829681b49cdd9b0505c15f0
9Anya9 <annasaliy909@gmail.com>
2021-02-22 16:14:53 +0200

    Improve code in actions and reducers

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,5 +1,4 @@
-// const SET_ITEMS = `SET_ITEMS_${ENTITY}`;
-export const SET_ITEMS = 'SET_ITEMS';
+export const SET_ITEMS = `SET_ITEMS`;
 export const SET_ITEM = 'SET_ITEM';
 export const SET_PAGE_SIZE = 'SET_PAGE_SIZE';
 export const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
@@ -8,18 +7,16 @@ export const SET_FILTER_STRING = 'SET_FILTER_STRING';
 
 
 function makeActionCreator(type, ...argNames) {
-   
-    return function(...args) {
-      let action = { type }
+    return function(entity, ...args) {
+      let action = {type: `${type}_${entity}`} 
       argNames.forEach((arg, index) => {
         action[argNames[index]] = args[index]
       })
-      
       return action
     }
   }
 
-export const setItems =  makeActionCreator(SET_ITEMS, 'data')
+export const setItems = makeActionCreator(SET_ITEMS,  'data')
 export const setItem =  makeActionCreator(SET_ITEM, 'data')
 export const setPageSize =  makeActionCreator(SET_PAGE_SIZE, 'pageSize')
 export const setFilterString =  makeActionCreator(SET_FILTER_STRING, 'filterString')

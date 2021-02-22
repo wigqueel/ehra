ebdff8bc23d1203ae3a6ee9c746bfbe5e947bb80
Mikhaylo <prreha@gmail.com>
2021-02-22 16:40:29 +0200

    public output page info

--- src/redux/public-reducer.js
+++ src/redux/public-reducer.js
@@ -2,11 +2,13 @@ import {api} from '../api/api';
 import { showNotification } from '../parts/Admin/utils/notifications/notifications';
 
 const SET_ACTIVE_THEME = 'SET_ACTIVE_THEME';
+const SET_PAGE_DATA = 'SET_PAGE_DATA';
 
 const initialState = {
     activeThemeId: null,
     activeThemeName: null,
     activeThemeContent: null,
+    pageData: null,
 }
 
 const publicReducer = (state = initialState, action) => {
@@ -18,6 +20,11 @@ const publicReducer = (state = initialState, action) => {
                 activeThemeName: action.name,
                 activeThemeContent: action.content,
             }
+        case SET_PAGE_DATA:
+            return {
+                ...state,
+                pageData: action.payload,
+            }
         default:
             return state;
     }
@@ -28,6 +35,11 @@ const setActiveTheme = (id, name, content) => ({
     id,
     name,
     content
+});
+
+const setPageData = (payload) => ({ 
+    type: SET_PAGE_DATA, 
+    payload
 });
 
 export const getActiveTheme = () => {
@@ -46,6 +58,25 @@ export const getActiveTheme = () => {
             console.log(error)
         } finally {
             
+        }
+
+    }
+};
+
+export const getPageByUrl = (url) => {
+    return async (dispatch) => {
+        try {
+            let response = await api.get(`/pages/view-by-url${url}`);
+            dispatch(setPageData(response.data));
+        } catch (error) {
+            if (error.response?.data?.message) {
+                showNotification(error.response.data.message, 'danger', 'shifted');
+            } else {
+                showNotification('Some error occurred', 'danger', 'shifted');
+            }
+            console.log(error)
+        } finally {
+
         }
 
     }
